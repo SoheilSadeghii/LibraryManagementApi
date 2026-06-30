80af53ecc6e5b8ee3e0073d517871bb34a5bbe94
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2026-06-30 18:41:32 +0330

    feat: add Author DTOs

---
+++ LibraryManagementApi/DTOs/Author/AuthorDto.cs
@@ -0,0 +1,9 @@
+﻿namespace LibraryManagementApi.DTOs.Author
+{
+    public class AuthorDto
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+    }
+}
---
+++ LibraryManagementApi/DTOs/Author/UpdateAuthorDto.cs
@@ -0,0 +1,8 @@
+﻿namespace LibraryManagementApi.DTOs.Author
+{
+    public class UpdateAuthorDto
+    {
+        public string Fullname { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+    }
+}
