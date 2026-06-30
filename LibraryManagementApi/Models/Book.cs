98d563a36525bde46491995a39c9620b0cb29ca9
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2026-06-30 17:37:53 +0330

    refactor: use decimal for Price and initialize string properties

--- LibraryManagementApi/Models/Book.cs
+++ LibraryManagementApi/Models/Book.cs
@@ -5,10 +5,10 @@ namespace LibraryManagementApi.Models
     public class Book
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public int year { get; set; }
-        public float Price { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int Year { get; set; }
+        public decimal Price { get; set; }
         public int AuthorId { get; set; }
         public int CategoryId { get; set; }
     }
