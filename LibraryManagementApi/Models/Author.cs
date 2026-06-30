49b7710be644af0aa9845453e9c026f1dc278364
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2026-06-30 17:12:37 +0330

    feat: add Author model

---
+++ LibraryManagementApi/Models/Author.cs
@@ -0,0 +1,9 @@
+﻿namespace LibraryManagementApi.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+    }
+}
