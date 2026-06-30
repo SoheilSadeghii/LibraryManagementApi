43083de4279948b0054522f07fcafc6e1ca670ae
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2026-06-30 19:51:02 +0330

    chore: configure Swagger for API documentation

--- LibraryManagementApi/Program.cs
+++ LibraryManagementApi/Program.cs
@@ -4,6 +4,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
