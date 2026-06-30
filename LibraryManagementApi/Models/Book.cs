822777d0b4668fb4be5af5485ab9cee6cf59904e
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2026-06-30 17:09:37 +0330

    feat: add Book model

---
+++ LibraryManagementApi/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection.PortableExecutable;
+
+namespace LibraryManagementApi.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int year { get; set; }
+        public float Price { get; set; }
+        public int AuthorId { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
