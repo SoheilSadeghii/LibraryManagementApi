d47275a54b186aaceaa5daeb9754764727f5b638
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2026-06-30 19:07:33 +0330

    feat: add Authentication DTOs

---
+++ LibraryManagementApi/DTOs/Auth/LoginResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace LibraryManagementApi.DTOs.Auth
+{
+    public class LoginResponseDto
+    {
+        public string Token { get; set; } = string.Empty;
+        public DateTime Expiration { get; set; }
+    }
+}
