1e85ef5fd32ee3c937c6c9810bc0eb9132a9498b
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2026-07-08 23:37:09 +0330

    feat: add CategoriesController and data annotations to DTOs

--- LibraryManagementApi/DTOs/Categories/UpdateCategoryDto.cs
+++ LibraryManagementApi/DTOs/Categories/UpdateCategoryDto.cs
@@ -1,7 +1,11 @@
-﻿namespace LibraryManagementApi.DTOs.Category
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagementApi.DTOs.Category
 {
     public class UpdateCategoryDto
     {
+        [Required]
+        [StringLength(50, MinimumLength = 1)]
         public string Name { get; set; } = string.Empty;
     }
 }
