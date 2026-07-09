c59f54f8807854f8d8143062e2826a50d5c2ff27
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2026-07-09 17:54:25 +0330

    feat: add dynamic sorting extension

---
+++ LibraryManagementApi/Extensions/MySortExtention.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq.Expressions;
+
+namespace LibraryManagementApi.Extensions
+{
+    public static class MySortExtention
+    {
+        public static IQueryable<TEntity> orderByCustom<TEntity>
+            (this IQueryable<TEntity> items, string sortBy, string sortOrder)
+        {
+            var type = typeof(TEntity);
+            var expression2 = Expression.Parameter(type, "t");
+            var property = type.GetProperty(sortBy);
+            var expression1 = Expression.
+                MakeMemberAccess(expression2, property);
+            var Lambda = Expression.Lambda(expression1, expression2);
+            var result = Expression.Call(typeof(Queryable),
+                sortOrder == "desc" ? "OrderByDescending" : "OrderBy",
+                new Type[] { type, property.PropertyType },
+                items.Expression,
+                Expression.Quote(Lambda));
+
+            return items.Provider.CreateQuery<TEntity>(result);
+        }
+    }
+}
