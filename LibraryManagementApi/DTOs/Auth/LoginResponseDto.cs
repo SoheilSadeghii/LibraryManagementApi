fe88c29bc274ac2314f2190b10a0d70c6a16d196
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2026-07-13 22:34:30 +0330

    feat: add data annotations to authentication DTOs

--- LibraryManagementApi/DTOs/Auth/LoginResponseDto.cs
+++ LibraryManagementApi/DTOs/Auth/LoginResponseDto.cs
@@ -1,8 +1,12 @@
-﻿namespace LibraryManagementApi.DTOs.Auth
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagementApi.DTOs.Auth
 {
     public class LoginResponseDto
     {
+        [Required]
         public string Token { get; set; } = string.Empty;
+        [Required]
         public DateTime Expiration { get; set; }
     }
 }
