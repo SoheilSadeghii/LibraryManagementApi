b83f951867fb79cdbfb5d6808dede6c2aaef2ac6
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2026-07-17 15:03:19 +0330

    feat: add user registration endpoint

--- LibraryManagementApi/Controllers/AuthController.cs
+++ LibraryManagementApi/Controllers/AuthController.cs
@@ -1,6 +1,9 @@
-﻿using LibraryManagementApi.Repositories;
+﻿using LibraryManagementApi.DTOs.Auth;
+using LibraryManagementApi.Models;
+using LibraryManagementApi.Repositories;
 using LibraryManagementApi.Services.Interfaces;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LibraryManagementApi.Controllers
@@ -16,6 +19,41 @@ namespace LibraryManagementApi.Controllers
         {
             _userRepository = userRepository;
             _jwtService = jwtService;
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register(RegisterRequestDto registerRequestDto)
+        {
+            var usernamerExists =
+                _userRepository.GetByUsername(registerRequestDto.Username);
+
+            if (usernamerExists != null) return BadRequest("Username already exists.");
+
+            var emailExists =
+                _userRepository.GetByEmail(registerRequestDto.Email);
+
+            if (emailExists != null) return BadRequest("Email already exists.");
+
+            var user = new User
+            {
+                Username = registerRequestDto.Username,
+                Email = registerRequestDto.Email,
+                Role = "User"
+            };
+
+            var passwordHasher = new PasswordHasher<User>();
+
+            user.PasswordHash =
+                passwordHasher.HashPassword(
+                    user,
+                    registerRequestDto.Password);
+
+            _userRepository.Create(user);
+
+            return CreatedAtAction(
+                nameof(Register),
+                new { id = user.Id },
+                null);
         }
     }
 }
